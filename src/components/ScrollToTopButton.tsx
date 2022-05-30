7dae2062b3d89169ef5c2701ca2a03d5ac74c4a4
Enes Sadık Özbek <es.ozbek@outlook.com>
2022-05-30 12:19:13 +0300

    Hide scroll to top if on top

--- src/components/ScrollToTopButton.tsx
+++ src/components/ScrollToTopButton.tsx
@@ -1,18 +1,26 @@
 import * as React from 'react'; 
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import clsx from 'clsx';
 
 export const ScrollToTopButton = () => {
+    const [showScrollButton, setShowScrollButton] = React.useState(false);;
     
+    React.useEffect(() => {
+        document.addEventListener('scroll', () => {
+            setShowScrollButton(window.pageYOffset > 300);
+        });
+    }, [])
+
     const scrollToTop = () => {
         window.scrollTo({
             top: 0,
-            behavior: 'smooth'
+            behavior: 'smooth' 
         });
     };
-    
+
     return (
         <button
-        className="scroll-to-top-button"
+        className={clsx("scroll-to-top-button", showScrollButton ? null : 'hidden')}
         onClick={scrollToTop}
         >
         <span className="icon">
